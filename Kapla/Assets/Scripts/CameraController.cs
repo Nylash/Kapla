20d1b0054d4bd4fd012b99653d661bcc3ca16b0f
Ben <ben.peltier7@gmail.com>
2019-11-12 17:06:34 +0100

    Init Unity Project

---
+++ Kapla/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float cameraSpeed = 3;
+    [SerializeField] int maxAngle = 80;
+    [SerializeField] int minAngle = 10;
+
+    GameObject center;
+
+    private void Start()
+    {
+        center = transform.parent.gameObject;
+    }
+
+    private void LateUpdate()
+    {
+        float xRot = cameraSpeed * Input.GetAxis("CamVertical") * Time.deltaTime;
+        float yRot = cameraSpeed * Input.GetAxis("CamHorizontal") * Time.deltaTime;
+
+        if(!(center.transform.localRotation.eulerAngles.x + xRot < minAngle) && !(center.transform.localRotation.eulerAngles.x + xRot > maxAngle))
+            center.transform.Rotate(xRot, 0f, 0f, Space.Self);
+        center.transform.Rotate(0f, yRot, 0f, Space.World);
+
+        //https://gamedev.stackexchange.com/questions/136174/im-rotating-an-object-on-two-axes-so-why-does-it-keep-twisting-around-the-thir
+    }
+}
---
+++ Kapla/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    [SerializeField] float movementSpeed=3;
+
+    Camera mainCamera;
+    
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        Vector3 right = transform.InverseTransformDirection(mainCamera.transform.right);
+        right.Normalize();
+        Vector3 desiredMoveDirection = Vector3.up * verticalAxis + right * horizontalAxis;
+        transform.Translate(desiredMoveDirection * movementSpeed * Time.deltaTime);
+    }
+}
