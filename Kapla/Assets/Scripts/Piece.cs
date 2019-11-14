f97906e329b5884dc886491f226f6f0535138429
Ben <ben.peltier7@gmail.com>
2019-11-14 17:26:32 +0100

    Line help for placing

--- Kapla/Assets/Scripts/Piece.cs
+++ Kapla/Assets/Scripts/Piece.cs
@@ -7,13 +7,15 @@ public class Piece : MonoBehaviour
 {
     [Header("PIECE CONFIGURATION")]
     public PieceType type;
-
+    [HideInInspector]
+    public LineRenderer line;
     Rigidbody rigid;
     Collider coll;
     MeshRenderer meshRender;
     Material pieceOriginalMaterial;
 
     bool toPlace;
+    RaycastHit hit;
 
     private void Start()
     {
@@ -25,15 +27,39 @@ public class Piece : MonoBehaviour
         meshRender = GetComponent<MeshRenderer>();
         pieceOriginalMaterial = meshRender.material;
         toPlace = true;
+        gameObject.AddComponent<LineRenderer>();
+        line = GetComponent<LineRenderer>();
+        line.material = GameManager.instance.cantDropMaterial;
+        line.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        line.startWidth = .05f;
+        line.endWidth = .05f;
     }
 
     public void Drop()
     {
         GameManager.instance.movingScript.currentPiece = null;
+        Destroy(line);
         rigid.useGravity = true;
         rigid.isKinematic = false;
         coll.isTrigger = false;
         toPlace = false;
+        if (meshRender.material != pieceOriginalMaterial)
+            meshRender.material = pieceOriginalMaterial;
+    }
+
+    private void Update()
+    {
+        if (toPlace)
+        {
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit, Mathf.Infinity))
+            {
+                if (line)
+                {
+                    line.SetPosition(0, transform.position);
+                    line.SetPosition(1, hit.point);
+                }
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
