e9cc10e896de1e80f460a49a42daf119227ec545
Chauveau <martin.chauveau@orange.fr>
2019-12-12 15:24:45 +0100

    bomb train

---
+++ Kapla/Assets/Scripts/Bomb.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject bomb;
+    public float power = 10.0f;
+    public float radius = 10.0f;
+    public float upforce = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "BombTrigger")
+        {
+            Debug.Log("ALAOUGBA");
+            Detonate();
+        }
+    }
+
+    void Detonate()
+    {
+        Vector3 explosionPosition = bomb.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPosition, radius, upforce, ForceMode.Impulse);
+            }
+        }
+    }
+
+}
