40b6f70e92e38536485acc95d38496e1851fa7a6
Ben <ben.peltier7@gmail.com>
2019-12-18 17:43:17 +0100

    Splashscreen

---
+++ Kapla/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    private void Start()
+    {
+        //temp to remove unity's debug updater which crashes with new input
+        GameObject go = GameObject.Find("[Debug Updater]");
+        if (go != null) DestroyImmediate(go);
+    }
+
+        void LoadLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
