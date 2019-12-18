e5a970f94c50d49b36117a850a7f2079bdb8b01d
Ben <ben.peltier7@gmail.com>
2019-12-18 16:54:56 +0100

    Arena randomizer

---
+++ Kapla/Assets/Scripts/ArenaRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaRandomizer : MonoBehaviour
+{
+#pragma warning disable 0649
+    [Header("ARENA PREFABS")]
+    [SerializeField] GameObject[] arenaPrefabs;
+#pragma warning restore 0649
+
+    private void Awake()
+    {
+        Instantiate(arenaPrefabs[Random.Range(0, arenaPrefabs.Length)],Vector3.zero, Quaternion.identity);
+    }
+}
