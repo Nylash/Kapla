a35be9b677a349c7db92269c9fa77914ee511c0e
Ben <ben.peltier7@gmail.com>
2019-12-20 16:02:56 +0100

    Lobby WIP

---
+++ Kapla/Assets/Scripts/PlayersNumberSelection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayersNumberSelection : MonoBehaviour
+{
+    public GameObject playersNumberObject;
+
+    private void Move()
+    {
+        print("zouglou");
+        if (playersNumberObject.activeSelf)
+        {
+
+        }
+    }
+}
