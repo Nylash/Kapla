2ff27a3f06cf507908b3b9c50598348e65cc61ba
Ben <ben.peltier7@gmail.com>
2020-01-14 17:02:53 +0100

    Bomb + Props room

--- Kapla/Assets/Scripts/Bomb.cs
+++ Kapla/Assets/Scripts/Bomb.cs
@@ -4,38 +4,50 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    public GameObject bomb;
-    public float power = 10.0f;
+    public float power = 1000.0f;
     public float radius = 10.0f;
     public float upforce = 1.0f;
     public List<Rigidbody> rigidList = new List<Rigidbody>();
+    public List<Piece> piecesList = new List<Piece>();
 
-    private void OnTriggerEnter(Collider other)
+    public void Detonate()
     {
-        if (other.tag == "BombTrigger")
-        {
-            print("fdp");
-            Detonate();
-        }
-    }
-
-    void Detonate()
-    {
-        /*Vector3 explosionPosition = bomb.transform.position;
+        Vector3 explosionPosition = transform.position;
         Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
         foreach (Collider item in colliders)
         {
-            if (!rigidList.Contains(item.transform.parent.gameObject.GetComponent<Rigidbody>()))
-                rigidList.Add(item.transform.parent.gameObject.GetComponent<Rigidbody>());
-        }
-        foreach (Collider hit in colliders)
-        {
-            Rigidbody rb = hit.GetComponent<Rigidbody>();
-            if (rb != null)
+            if(item.GetType() == typeof(MeshCollider))
             {
-                rb.AddExplosionForce(power, explosionPosition, radius, upforce, ForceMode.Impulse);
+                if (!rigidList.Contains(item.attachedRigidbody))
+                {
+                    rigidList.Add(item.attachedRigidbody);
+                    if (item.attachedRigidbody.GetComponent<TrainRigidbody>())
+                    {
+                        if (!piecesList.Contains(item.attachedRigidbody.transform.parent.GetComponent<Piece>()))
+                            piecesList.Add(item.attachedRigidbody.transform.parent.GetComponent<Piece>());
+                    }
+                    else
+                    {
+                        if (!item.attachedRigidbody.GetComponent<Piece>().isBomb)
+                        {
+                            if (!piecesList.Contains(item.attachedRigidbody.GetComponent<Piece>()))
+                                piecesList.Add(item.attachedRigidbody.GetComponent<Piece>());
+                        }                            
+                    }
+                }
             }
-        }*/
+        }
+        foreach (Piece item in piecesList)
+        {
+            item.Explode(power, transform.position, radius, upforce);
+        }
+        GameManager.instance.AllPieces.Remove(gameObject);
+        Destroy(gameObject);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
 }
